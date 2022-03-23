e2e6a8760cb37ff553423edb2dabf9041b4a50ec
Arran Ubels <arran4@gmail.com>
2022-03-23 23:04:39 +1100

    Local testing

---
+++ parserlocal_test.go
@@ -0,0 +1,40 @@
+package rcs
+
+import (
+	"bytes"
+	"embed"
+	"github.com/google/go-cmp/cmp"
+	"io/fs"
+	"path"
+	"testing"
+)
+
+var (
+	//go:embed "testdata/local/*,v"
+	localTests embed.FS
+)
+
+func TestParseLocalFiles(t *testing.T) {
+	dir := "testdata/local"
+	d, err := localTests.ReadDir(dir)
+	if err != nil {
+		t.Fatalf("Error: %s", err)
+	}
+	for _, tt := range d {
+		t.Run(tt.Name(), func(t *testing.T) {
+			b, err := fs.ReadFile(localTests, path.Join(dir, tt.Name()))
+			if err != nil {
+				t.Errorf("ReadFile() error = %s", err)
+				return
+			}
+			got, err := ParseFile(bytes.NewReader(b))
+			if err != nil {
+				t.Errorf("ParseFile() error = %s", err)
+				return
+			}
+			if diff := cmp.Diff(got.String(), string(b)); diff != "" {
+				t.Errorf("String(): %s", diff)
+			}
+		})
+	}
+}
